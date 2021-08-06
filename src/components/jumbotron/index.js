daad319fc6e8ac2a9ed367c87a87ff948981cf1a
root <root@LAPTOP-L2B4NHCF.localdomain>
2021-08-07 01:25:05 +0530

    first_commit

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,28 @@
+
+import {Inner,Container,Pane,Title,SubTitle,Image, Item} from './styles/jumbotron';
+
+export default function Jumbotron({children, direction ="row", ...restprops}) {
+    return (<Item  {...restprops}>
+        <Inner direction={direction}>
+            {children}
+        </Inner>
+    </Item>)
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    return <Container { ...restProps} >{children}</Container>
+}
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+  };
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+};
+  
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+  
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps} />;
+};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './app';
+import 'normalize.css';
+import {GlobalStyles} from './global-styles';
+render(<>
+<GlobalStyles />
+<App />
+</>,
+document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
