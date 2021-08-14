14cdeefc241c78425495237f59e6417c2e00d6ba
rishi741 <rishabhchirania@gmail.com>
2021-08-15 02:12:57 +0530

    signup and route

--- src/app.js
+++ src/app.js
@@ -2,20 +2,39 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Home, Browse, Signin,Signup} from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 //import HeaderContainer from './containers/header';
+
+
 export default function App() {
+  const user = {};
   return (
   <Router>
-    <Route exact path={ROUTES.SIGN_IN}>
-      <Signin />
-    </Route>
+      <IsUserRedirect 
+      user={user}
+      loggedInPath={ROUTES.BROWSE}
+      path={ROUTES.SIGN_IN}
+      exact
+      >
+
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect 
+      user={user}
+      loggedInPath={ROUTES.BROWSE}
+      path={ROUTES.SIGN_UP}
+      exact
+      >
+        <Signup />
+      </IsUserRedirect>
+
+
+
+
     <Route exact path={ROUTES.BROWSE}>
       <Browse />
     </Route>
-    <Route exact path={ROUTES.SIGN_UP}>
-      <Signup />
 
-    </Route>
     <Route exact path={ROUTES.HOME}>
       <Home />
     </Route>
